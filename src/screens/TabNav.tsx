8e3d96904a9d29734bf8bf6ecfea0efd713c087f
hyunsung <hslgood@gmail.com>
2020-02-04 11:44:49 +0900

    Add Tab navigator (footer)

---
+++ src/screens/TabNav.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+// import { Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { AntDesign } from '@expo/vector-icons';
+
+import MainScreen from './MainScreen';
+import Favor from './Favor';
+import Search from './Search';
+import Chat from './Chat';
+import UserInfo from './UserInfo';
+
+interface IconProps {
+  focused: boolean;
+  tintColor: string | undefined;
+}
+
+const TabNav = createBottomTabNavigator(
+  {
+    Home: MainScreen,
+    Favor,
+    Search,
+    Chat,
+    UserInfo,
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ tintColor }: IconProps): JSX.Element => {
+        const { routeName } = navigation.state;
+        let iconName;
+        switch (routeName) {
+          case 'Home':
+            iconName = 'home';
+            break;
+          case 'Favor':
+            iconName = 'heart';
+            break;
+          case 'Search':
+            iconName = 'search1';
+            break;
+          case 'Chat':
+            iconName = 'message1';
+            break;
+          case 'UserInfo':
+            iconName = 'user';
+            break;
+          default:
+            iconName = 'exclamationcircleo';
+        }
+        return <AntDesign name={iconName} size={28} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: 'purple',
+      inactiveTintColor: 'gray',
+      showLabel: false,
+    },
+  },
+);
+
+export default createAppContainer(TabNav);
