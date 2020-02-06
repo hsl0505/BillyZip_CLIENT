e6cd4a35a68317c3d52204546fdb2c0e1270b3d9
hyunsung <hslgood@gmail.com>
2020-02-06 21:54:46 +0900

    Add mobile valid component

---
+++ src/components/SignUp/MobileValidInput.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
+import { withNavigation, NavigationInjectedProps } from 'react-navigation';
+
+function MobileValidInput(
+  props: Partial<NavigationInjectedProps>,
+): JSX.Element {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>핸드폰 인증</Text>
+      <Button
+        title="임시 인증완료버튼"
+        onPress={(): boolean => {
+          if (props.navigation) {
+            props.navigation.navigate('SignUpInputScreen');
+          }
+          return false;
+        }}
+      />
+    </View>
+  );
+}
+export default withNavigation(MobileValidInput);
