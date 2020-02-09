197d0005f31e437dcb6b49b8786a11263543ac92
hyunsung <hslgood@gmail.com>
2020-02-09 22:37:24 +0900

    Add props and props type in RecommendEntry

--- src/components/MainScreen/Recommend.tsx
+++ src/components/MainScreen/Recommend.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-
 import RecommendEntry from './RecommendEntry';
 
-function Recommend(): JSX.Element {
+interface Props {
+  rank: undefined | object[];
+}
+
+function Recommend(props: Props): JSX.Element {
+  const { rank } = props;
   return (
     <View style={{ flex: 1 }}>
       <View style={{ flex: 1 }}>
-        <Text style={{ fontSize: 24, marginTop: 10, marginLeft: 10 }}>
+        <Text style={{ fontSize: 24, marginTop: 10, marginLeft: 15 }}>
           BillyZip이 추천하는 House
         </Text>
       </View>
       <View style={{ flex: 1 }}>
-        <RecommendEntry />
+        <RecommendEntry rank={rank} />
       </View>
     </View>
   );
