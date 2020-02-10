17e783aa90504cac80f650eda6e699e4f63c58c9
hyunsung <hslgood@gmail.com>
2020-02-10 21:26:22 +0900

    Add search 주석

--- src/screens/Search.tsx
+++ src/screens/Search.tsx
@@ -9,6 +9,11 @@ const styles = StyleSheet.create({
   },
 });
 
+// 스택 네비게이션을 쌓아주셔야됩니다
+// 1 : 버튼 분기 스크린
+// 2 스크린 : 검색
+// 3 스크린 : 지도 검색
+
 function Search(): JSX.Element {
   return (
     <View style={styles.container}>
