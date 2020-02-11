47056abeb6602fda952458ecdfd964230a9ea60b
hyunsung <hslgood@gmail.com>
2020-02-12 04:48:28 +0900

    Add touchable opacity and navigate

--- src/components/MainScreen/MoreCardComponent.tsx
+++ src/components/MainScreen/MoreCardComponent.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import { Text, View, ActivityIndicator } from 'react-native';
+import { Text, View, ActivityIndicator, TouchableOpacity } from 'react-native';
 import { Card, Rating } from 'react-native-elements';
+import {
+  withNavigation,
+  NavigationScreenProp,
+  NavigationRoute,
+  NavigationParams,
+} from 'react-navigation';
 
 interface Props {
   item: {
+    id: number;
     title: string;
     description: string;
     images: Images[];
     avgRating: number;
   };
+  navigation: NavigationScreenProp<
+    NavigationRoute<NavigationParams>,
+    NavigationParams
+  >;
 }
 
 interface Images {
@@ -17,38 +28,43 @@ interface Images {
 
 function MoreCardComponent(props: Props): JSX.Element {
   const { item } = props;
-  const { title, description, images, avgRating } = item;
+  const { id, title, description, images, avgRating } = item;
   const { filePath } = images[0];
 
-  //   console.log('순차 렌더링', id);
   return (
-    <Card
-      image={{ uri: filePath }}
-      containerStyle={{ marginBottom: 15, height: 300 }}
-      imageStyle={{ height: 210 }}
-      imageProps={{
-        resizeMode: 'cover',
-        PlaceholderContent: <ActivityIndicator />,
+    <TouchableOpacity
+      onPress={(): void => {
+        props.navigation.navigate('HouseDetail', { houseId: id });
       }}
     >
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-        <View>
-          <Text>{title}</Text>
-          <Text>{description}</Text>
+      <Card
+        image={{ uri: filePath }}
+        containerStyle={{ marginBottom: 15, height: 300 }}
+        imageStyle={{ height: 210 }}
+        imageProps={{
+          resizeMode: 'cover',
+          PlaceholderContent: <ActivityIndicator />,
+        }}
+      >
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          <View>
+            <Text>{title}</Text>
+            <Text>{description}</Text>
+          </View>
+          <View>
+            <Text style={{ alignSelf: 'center' }}>Rating</Text>
+            <Rating
+              readonly
+              startingValue={avgRating}
+              imageSize={20}
+              fractions={2}
+            />
+            <Text style={{ alignSelf: 'center' }}>{avgRating}</Text>
+          </View>
         </View>
-        <View>
-          <Text style={{ alignSelf: 'center' }}>Rating</Text>
-          <Rating
-            readonly
-            startingValue={avgRating}
-            imageSize={20}
-            fractions={2}
-          />
-          <Text style={{ alignSelf: 'center' }}>{avgRating}</Text>
-        </View>
-      </View>
-    </Card>
+      </Card>
+    </TouchableOpacity>
   );
 }
 
-export default React.memo(MoreCardComponent);
+export default React.memo(withNavigation(MoreCardComponent));
