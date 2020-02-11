d46088284b277ac2d1cc74d5ba6d45737b0456f3
hyunsung <hslgood@gmail.com>
2020-02-12 04:23:41 +0900

    Add review screen

---
+++ src/screens/HouseDetail/ReviewScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function ReviewScreen(): JSX.Element {
+  return (
+    <View style={{ flex: 1, backgroundColor: '#fff' }}>
+      <Text>리뷰스크린</Text>
+    </View>
+  );
+}
+
+export default ReviewScreen;
