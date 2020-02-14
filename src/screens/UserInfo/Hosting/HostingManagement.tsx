3411a4fd907764393c952defe358b270e5421545
hyunsung <hslgood@gmail.com>
2020-02-14 22:13:03 +0900

    Add hosting management screen

---
+++ src/screens/UserInfo/Hosting/HostingManagement.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function HostingManagement(): JSX.Element {
+  return (
+    <View>
+      <Text>호스팅 매니지먼트 스크린</Text>
+    </View>
+  );
+}
+
+export default HostingManagement;
