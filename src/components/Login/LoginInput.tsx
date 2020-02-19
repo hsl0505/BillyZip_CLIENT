e254a21dc2f5abe3247feeb930492c9453d0c890
hyunsung <hslgood@gmail.com>
2020-02-19 23:37:09 +0900

    Add userid and username ay async storage

--- src/components/Login/LoginInput.tsx
+++ src/components/Login/LoginInput.tsx
@@ -60,7 +60,13 @@ function LoginInput(props: Partial<NavigationInjectedProps>): JSX.Element {
             })
             .then((res) => {
               if (res.status === 200) {
+                console.log(res.data);
                 asyncStorageHelper.setItem('userToken', res.data.token);
+                asyncStorageHelper.setItem(
+                  'userId',
+                  JSON.stringify(res.data.userId),
+                );
+                asyncStorageHelper.setItem('userName', res.data.userName);
                 if (props.navigation) {
                   props.navigation.navigate('App');
                 }
