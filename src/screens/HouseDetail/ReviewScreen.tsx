616f92a954af5c92f1c5c6b79c66440fed24feb1
hyunsung <hslgood@gmail.com>
2020-02-19 13:58:03 +0900

    Add props type for review component

--- src/screens/HouseDetail/ReviewScreen.tsx
+++ src/screens/HouseDetail/ReviewScreen.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import { View } from 'react-native';
+import {
+  withNavigation,
+  NavigationScreenProp,
+  NavigationRoute,
+  NavigationParams,
+} from 'react-navigation';
 import ReviewComponent from '../../components/Reviews/ReviewComponent';
 
-function ReviewScreen(): JSX.Element {
+interface Props {
+  navigation: NavigationScreenProp<
+    NavigationRoute<NavigationParams>,
+    NavigationParams
+  >;
+}
+
+function ReviewScreen(props: Props): JSX.Element {
   // 리뷰스, 평균 레이팅 받기
+  const { navigation } = props;
+  const avgRating = navigation.getParam('avgRating');
+  const reviews = navigation.getParam('reviews');
   return (
     <View style={{ flex: 1, backgroundColor: '#fff' }}>
-      <ReviewComponent />
+      <ReviewComponent avgRating={avgRating} reviews={reviews} />
     </View>
   );
 }
 
-export default ReviewScreen;
+export default withNavigation(ReviewScreen);
