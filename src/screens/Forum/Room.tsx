75f09d6866c03d5aa696caef25db92e765912de4
Jo Seong-cheol <josworks27@gmail.com>
2020-02-20 03:29:59 +0900

    Modify Forum

--- src/screens/Forum/Room.tsx
+++ src/screens/Forum/Room.tsx
@@ -11,30 +11,43 @@ let myName: any;
 
 function Room(props: any): JSX.Element {
   // ! 매물상세에서 send a message를 눌러 이 컴포넌트로 들어올 때 props로 호스트의 hostId를 받는다.
-  // const { hostId } = props;
-  const hostId = 1;
+  let { hostId } = props;
+  const { navigation } = props;
+  const data = navigation ? navigation.getParam('data') : undefined;
+
+  console.log('data', data);
+  console.log('??', hostId);
+  // const hostId = 1;
+
+
+  // 풋터에서 왔다면 hostId 없기 때문에 hostId = data; 해준다.
+  if (!hostId) {
+    hostId = data;
+  }
 
   const [chat, setChat] = useState('');
   const [messages, setMessages] = useState([]);
 
-  console.log('myId is ', myId);
-
-  // ! 모든 유저는 자신만의 룸이 있다.
-
-  // ! 문의할 떄는 다른 사람의 룸에 조인한다.
-  // ! 문의할 떄는 props로 hostId 받고 들어오기 때문에 아래 코드가 실행된다.
-  // ! 문의가 아닌 경우는 props로 받지 않기 때문에 실행되지 않는다.
-  if (hostId) {
-    socket.emit('joinRoom', hostId);
-  }
-
   useEffect(() => {
+    axiosInstance
+      .post('forum/room', {
+        hostId,
+      })
+      .then((res) => {
+        setMessages(JSON.parse(res.data.forumLog));
+      })
+      .catch((err) => console.log(err));
+
     const getMyId = async (): Promise<void> => {
       myId = await AsyncStorage.getItem('userId');
       myName = await AsyncStorage.getItem('userName');
       socket.emit('joinRoom', myId);
     };
     getMyId();
+
+    if (hostId) {
+      socket.emit('joinRoom', hostId);
+    }
 
     socket.on('chat', (msg: any) => {
       console.log(msg);
@@ -43,7 +56,7 @@ function Room(props: any): JSX.Element {
         return prev.concat(msg);
       });
     });
-  }, []);
+  }, [hostId]);
 
   const handleMessageToHost = (roomId: number): void => {
     socket.emit('chat', roomId, chat, myName);
@@ -67,16 +80,6 @@ function Room(props: any): JSX.Element {
       <Button
         title="전송"
         onPress={(): any => {
-          // ! 문의하기 위해 이 스크린에 들어왔으면 hostId가 있다. => hostId
-          // ! 아니라면 houtId 없기 때문에 => myId
-          handleMessageToHost(hostId || myId);
-        }}
-      />
-      <Button
-        title="나가기"
-        onPress={(): any => {
-          console.log('나가기~');
-          // 서버에 요청보내서 서버에서 채팅로그 저장
           axiosInstance
             .post('forum', {
               messages,
@@ -85,11 +88,18 @@ function Room(props: any): JSX.Element {
             })
             .then((res) => {
               if (res.status === 200) {
-                // 그리고 forum 스크린으로 네비게이트
-                props.navigation.navigate('Forum');
+                console.log('전송 성공');
               }
             })
             .catch((err) => console.error(err));
+
+          handleMessageToHost(hostId || myId);
+        }}
+      />
+      <Button
+        title="포럼 리스트로 나가기"
+        onPress={(): any => {
+          props.navigation.navigate('Forum');
         }}
       />
     </View>
