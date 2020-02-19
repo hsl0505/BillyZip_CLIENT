7721a4d4bd92ba2357d441696dc2b55436db3f22
Jo Seong-cheol <josworks27@gmail.com>
2020-02-19 15:17:43 +0900

    Add No search reulst form

--- src/components/Search/NormalResult.tsx
+++ src/components/Search/NormalResult.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
-import { View, FlatList } from 'react-native';
+import { View, FlatList, Text, StyleSheet } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 import MoreCardComponent from '../MainScreen/MoreCardComponent';
+
+// const styles = StyleSheet.create({
+//   container: {
+//     alignItems: 'center',
+//     flex: 1,
+//     justifyContent: 'center',
+//     marginTop: 20,
+//   },
+// });
 
 interface Props {
   data: Item[];
@@ -24,14 +34,28 @@ function NormalResult(props: Props): JSX.Element {
   const { data } = props;
   return (
     <View>
-      <FlatList
-        data={data}
-        renderItem={({ item }): JSX.Element => (
-          <MoreCardComponent item={item} isFav="a" />
-        )}
-        keyExtractor={(item): string => item.id.toString()}
-        windowSize={3}
-      />
+      {data.length > 0 ? (
+        <FlatList
+          data={data}
+          renderItem={({ item }): JSX.Element => (
+            <MoreCardComponent item={item} isFav="a" />
+          )}
+          keyExtractor={(item): string => item.id.toString()}
+          windowSize={3}
+        />
+      ) : (
+        <View
+          style={{
+            alignItems: 'center',
+            marginTop: 100,
+          }}
+        >
+          <AntDesign name="frowno" size={80} color="purple" />
+          <Text style={{ fontSize: 20, marginTop: 50 }}>
+            조건에 맞는 집이 없습니다.
+          </Text>
+        </View>
+      )}
     </View>
   );
 }
