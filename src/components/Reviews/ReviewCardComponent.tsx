2983c3f1805c88f3efdc7387b329d617191f67db
hyunsung <hslgood@gmail.com>
2020-02-22 22:09:46 +0900

    Change review -> card form

--- src/components/Reviews/ReviewCardComponent.tsx
+++ src/components/Reviews/ReviewCardComponent.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Card } from 'react-native-elements';
 import { AntDesign } from '@expo/vector-icons';
 
 interface Props {
@@ -23,45 +24,45 @@ function ReviewCardComponent(props: Props): JSX.Element {
     <View
       style={{
         flex: 1,
-        marginVertical: 20,
+        marginTop: 20,
+        marginBottom: 35,
         marginHorizontal: 20,
-        borderStyle: 'solid',
-        borderBottomWidth: 1,
-        borderBottomColor: '#D8D8D8',
       }}
     >
-      <View
-        style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-        }}
-      >
-        <Text style={{ fontWeight: 'bold', fontSize: 20 }}>
-          {name} 님의 리뷰
-        </Text>
+      <Card>
         <View
           style={{
             flexDirection: 'row',
             alignItems: 'center',
-            marginRight: 20,
+            justifyContent: 'space-between',
           }}
         >
-          <AntDesign name="star" size={17} />
-          <Text style={{ fontWeight: 'bold', fontSize: 17, marginLeft: 3 }}>
-            {rating}
+          <Text style={{ fontWeight: 'bold', fontSize: 20 }}>
+            {name} 님의 리뷰
           </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              marginRight: 20,
+            }}
+          >
+            <AntDesign name="star" size={17} color="purple" />
+            <Text style={{ fontWeight: 'bold', fontSize: 17, marginLeft: 3 }}>
+              {rating}
+            </Text>
+          </View>
         </View>
-      </View>
-      <View style={{ marginTop: 5 }}>
-        <Text style={{ color: '#A4A4A4' }}>{`${createdAt.slice(
-          0,
-          4,
-        )}년 ${createdAt.slice(5, 7)}월 ${createdAt.slice(8, 10)}일`}</Text>
-      </View>
-      <View style={{ marginTop: 30, marginBottom: 50 }}>
-        <Text style={{ fontSize: 18 }}>{comment}</Text>
-      </View>
+        <View style={{ marginTop: 5 }}>
+          <Text style={{ color: '#A4A4A4' }}>{`${createdAt.slice(
+            0,
+            4,
+          )}년 ${createdAt.slice(5, 7)}월 ${createdAt.slice(8, 10)}일`}</Text>
+        </View>
+        <View style={{ marginTop: 30, marginBottom: 50 }}>
+          <Text style={{ fontSize: 18 }}>{comment}</Text>
+        </View>
+      </Card>
     </View>
   );
 }
