ca19103bef71a0d4f27f2cd177b59ab0d03ecd94
BOMI KIM <bomedang@gmail.com>
2020-02-23 23:23:27 +0900

    Fix subscribe_screen & componenets

--- src/screens/UserInfo/CurrentHouse/Subscribe.tsx
+++ src/screens/UserInfo/CurrentHouse/Subscribe.tsx
@@ -1,41 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 
 import CurrentPlan from '../../../components/UserInfo/CurrentModel/CurrentPlan';
-// import LivingHouse from '../../../components/UserInfo/CurrentModel/LivingHouse';
 import axiosInstance from '../../../util/axiosInstance';
-
-/* 
-          <LivingHouse subscribedHouses={subscribedHouses} /> */
-
-// const styles = StyleSheet.create({
-//   // ButtonViewStyle: {
-//   //   backgroundColor: '#D1D1D1',
-//   //   // borderColor: '#dfe4ea',
-//   //   // borderWidth: 1,
-//   //   // marginBottom: 30,
-//   //   // marginLeft: 20,
-//   //   // marginRight: 20,
-//   //   // padding: 10,
-//   //   width: '90%',
-//   // },
-//   TextViewStyle: {
-//     backgroundColor: '#F9F9F9',
-//     borderColor: '#dfe4ea',
-//     borderWidth: 1,
-//     marginLeft: 20,
-//     marginRight: 20,
-//     padding: 10,
-//     width: '90%',
-//   },
-//   title: {
-//     fontSize: 15,
-//     marginBottom: 15,
-//     marginLeft: 20,
-//     marginRight: 20,
-//     marginTop: 20,
-//   },
-// });
 
 function Subscribe(): JSX.Element {
   const [subscribedHouses, setSubscribedHouses] = useState({});
@@ -56,7 +23,6 @@ function Subscribe(): JSX.Element {
         <View />
       ) : (
         <View>
-          <Text>현재 구독 중인 플랜 :</Text>
           <CurrentPlan subscribedHouses={subscribedHouses} />
         </View>
       )}
