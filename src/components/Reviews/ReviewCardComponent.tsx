8f363ead2eb5fd7e64a99e0f2ad44de06437c3de
hyunsung <hslgood@gmail.com>
2020-02-23 16:08:26 +0900

    Add review delete

--- src/components/Reviews/ReviewCardComponent.tsx
+++ src/components/Reviews/ReviewCardComponent.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { Card } from 'react-native-elements';
-import { AntDesign } from '@expo/vector-icons';
+import React, { useState } from 'react';
+import { View, Text, Alert } from 'react-native';
+import { Card, Overlay, Button } from 'react-native-elements';
+import {
+  withNavigation,
+  NavigationScreenProp,
+  NavigationRoute,
+  NavigationParams,
+} from 'react-navigation';
+import { AntDesign, FontAwesome } from '@expo/vector-icons';
+import axiosInstance from '../../util/axiosInstance';
 
 interface Props {
+  userId: number;
+  houseId: number;
   item: {
     id: number;
     comment: string;
     rating: number;
     createdAt: string;
     user: {
+      id: number;
       name: string;
     };
   };
+  navigation: NavigationScreenProp<
+    NavigationRoute<NavigationParams>,
+    NavigationParams
+  >;
 }
 
 function ReviewCardComponent(props: Props): JSX.Element {
-  const { item } = props;
+  const { item, userId, houseId } = props;
   const { comment, rating, createdAt, user } = item;
-  const { name } = user;
+  const reviewId = item.id;
+  const { name, id } = user;
+  const [isVisible, setVisible] = useState(false);
 
   return (
     <View
@@ -44,13 +60,31 @@ function ReviewCardComponent(props: Props): JSX.Element {
             style={{
               flexDirection: 'row',
               alignItems: 'center',
-              marginRight: 20,
+              marginRight: 10,
             }}
           >
             <AntDesign name="star" size={17} color="purple" />
-            <Text style={{ fontWeight: 'bold', fontSize: 17, marginLeft: 3 }}>
+            <Text
+              style={{
+                fontWeight: 'bold',
+                fontSize: 17,
+                marginLeft: 3,
+                marginRight: 20,
+              }}
+            >
               {rating}
             </Text>
+            {id === Number(userId) ? (
+              <FontAwesome
+                name="trash-o"
+                size={26}
+                onPress={(): void => {
+                  setVisible(true);
+                }}
+              />
+            ) : (
+              <View />
+            )}
           </View>
         </View>
         <View style={{ marginTop: 5 }}>
@@ -63,8 +97,49 @@ function ReviewCardComponent(props: Props): JSX.Element {
           <Text style={{ fontSize: 18 }}>{comment}</Text>
         </View>
       </Card>
+
+      <Overlay
+        isVisible={isVisible}
+        onBackdropPress={(): void => setVisible(false)}
+        height={100}
+        width={220}
+      >
+        <View style={{ marginHorizontal: 10 }}>
+          <View style={{ marginTop: 5 }}>
+            <Text>리뷰를 삭제하시겠습니까?</Text>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-around',
+              marginTop: 20,
+            }}
+          >
+            <Button
+              title="삭제"
+              type="clear"
+              onPress={(): void => {
+                axiosInstance
+                  .delete(`houses/${houseId}/reivew/${reviewId}`)
+                  .then(() => {
+                    setVisible(false);
+                    Alert.alert('리뷰가 삭제 되었습니다');
+                    props.navigation.goBack();
+                  });
+              }}
+            />
+            <Button
+              title="취소"
+              type="clear"
+              onPress={(): void => {
+                setVisible(false);
+              }}
+            />
+          </View>
+        </View>
+      </Overlay>
     </View>
   );
 }
 
-export default ReviewCardComponent;
+export default withNavigation(ReviewCardComponent);
